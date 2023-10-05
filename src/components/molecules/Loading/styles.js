ef51628ef29f00f994abcc883f13e28db94af4f1
DanyKriswana <daniriswana@gmail.com>
2023-10-05 11:53:36 +0700

    create loading feature and exporting

---
+++ src/components/molecules/Loading/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontFamily: 'Poppins-Regular',
+        fontSize: 18,
+        marginTop: 12
+    }
+}) 
